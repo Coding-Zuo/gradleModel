a1d8bf75c83d9f4d0c1e8394f21e7c5915e201e8
Coding-Zuo <zzuuoo666@sina.com>
2020-04-15 15:26:55 +0800

    跑起项目

--- src/main/java/com/zuo/model/ModelApplication.java
+++ src/main/java/com/zuo/model/ModelApplication.java
@@ -1,9 +1,13 @@
 package com.zuo.model;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("com.zuo.model.mapper")
 public class ModelApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/zuo/model/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.zuo.model.controller;
+
+
+import com.zuo.model.utils.common.mvc.AbstractController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController extends AbstractController {
+
+    @RequestMapping("/index")
+    public String Index() {
+        return "index";
+    }
+
+}
