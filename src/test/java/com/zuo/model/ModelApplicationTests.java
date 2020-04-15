95c0a8be131b088d056332c634168c2cf3474700
Coding-Zuo <zzuuoo666@sina.com>
2020-04-15 13:13:20 +0800

    first

---
+++ src/test/java/com/zuo/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuo.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
