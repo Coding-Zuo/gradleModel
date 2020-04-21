b3234ce355d8f021cf20a53594559a061115e9ed
Coding-Zuo <zzuuoo666@sina.com>
2020-04-21 14:46:08 +0800

    登录获得当前用户信息

--- src/main/java/com/zuo/model/controller/UserController.java
+++ src/main/java/com/zuo/model/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.zuo.model.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +16,18 @@ import java.util.Map;
 @Controller
 @RequestMapping("/admin/user")
 public class UserController {
+
+    @RequestMapping(value = "/me",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Object getCurrentUser(Authentication authentication){
+        return authentication;
+    }
+
+    @RequestMapping(value = "/me/detail",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Object detail(@AuthenticationPrincipal UserDetails user){
+        return user;
+    }
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String query1() {
